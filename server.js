cd1b95ce1283a695ff28bee3e28c496e95c6b562
Glitch (arrasiosiege) <none>
2022-03-21 19:54:06 +0000

    🗣️🚝 Updated with Glitch

--- server.js
+++ server.js
@@ -2906,7 +2906,7 @@ class Entity {
         case 'glidedef':
             this.maxSpeed = this.topSpeed;
             this.damp = 0.05;
-            this.COLOR = 35;
+            this.color = 35;
             break;
         case "grow":
             this.SIZE += 1.34;
