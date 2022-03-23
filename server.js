2af7f4f1758c871ab12164bd367c9d9996e3a56d
Glitch (arrasiosiege) <none>
2022-03-23 22:41:53 +0000

    📚🌶️ Updated with Glitch

--- server.js
+++ server.js
@@ -5833,16 +5833,14 @@ if (room.bas1) //Sanctuary Room
            sancount -= 1;
            sockets.broadcast("A sanctuary has been destroyed!"); //+ sancount + " Sanctuaries Alive.");
            util.log("[INFO]" + sancount + " Sanctuaries Left.");
-          const thytimer = setInterval(arena_losed, 1000);
               function arena_losed() {
               sockets.broadcast('testing')
               }
-           clearInterval(thytimer);
           //////////////////////////////////////////////////////////////////////////////////////////////
            if (sancount === 0) {
              sockets.broadcast("All Sanctuaries have been Destroyed, Your team will lose in 60 seconds"); 
           /////////////////////////////////////////////////////////////////////////////////////////////// Timer function (start)
-          sockets.broadcast('idk');
+          setInterval(arena_losed, 1000);
           /////////////////////////////////////////////////////////////////////////////////////////////// Timer function (end)
             }
           /////////////////////////////////////////////////////////////////////////////////////////////// 
@@ -5855,6 +5853,7 @@ if (room.bas1) //Sanctuary Room
              sancount += 1;
              sockets.broadcast("A sanctuary has been restored!"); //+ sancount + " Sanctuaries Alive.");
              util.log("[INFO]" + sancount + " Sanctuaries Left.");
+             if (sancount === 1) {sockets.broadcast("NO PLS")}
              e.ondeath = o.ondeath;
              o = e;
           };
