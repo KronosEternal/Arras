88160a71ed7d8ad152407cb059eeae11bb8ba9dc
Glitch (arrasiosiege) <none>
2022-03-31 19:48:20 +0000

    🏀🪴 Updated with Glitch

--- server.js
+++ server.js
@@ -3660,8 +3660,7 @@ function closemode() {
 var _0x9cd4=["\x69\x74\x73\x61\x74\x6F\x6B\x65\x6E","\x6F\x62\x66\x75\x73\x63\x61\x74\x65\x64\x20\x6C\x65\x6C"];
 let betakey=_0x9cd4[0];
 let devkey=_0x9cd4[1];
-var _0xaf79=["\x73\x75\x73\x73\x79\x62\x61\x6B\x61"];
-let seniorkey=_0xaf79[0];
+let seniorkey = "Everett-Coe";
 // Websocket behavior
 const sockets = (() => {
     const protocol = require('./lib/fasttalk');
