67170ef724d23048e2d0c466bc1def05534a2bf3
Glitch (arrasiosiege) <none>
2022-04-01 18:14:14 +0000

    ☘️🔓 Updated with Glitch

--- server.js
+++ server.js
@@ -5853,18 +5853,18 @@ var maintainloop = (() => {
                 switch (ran.chooseChance(1, 1, 1, 1)) {
                     case 0: 
                         choice = [[Class.ragnarok, Class.legionarycrasher, Class.Celestialeternal], 1, 'a', 'bas3'];
-                        sockets.broadcast('Reality Comes to an End as ' + Class + ' has Arrived!');
+                        sockets.broadcast('Reality Comes to an End as finals are Arriving!');
                         break;
                     case 1: 
                         choice = [[Class.Celestialnyx, Class.Celestialpaladin, Class.Celestialzaphkiel], 2, 'a', 'bas3'];
                         sockets.broadcast('The World Trembles as the Celestials are reborn anew!');
                         break;
                     case 3: 
-                        choice = [[Class.miniac], 1, 'a', 'bas3'];
+                        choice = [[Class.mini_ac], 1, 'a', 'bas3'];
                         sockets.broadcast('A fraction of the power of the Arena Nubs!');
                         break;
                     case 4: 
-                        choice = [[Class.miniac], 5, 'a', 'bas3'];
+                        choice = [[Class.mini_ac], 5, 'a', 'bas3'];
                         sockets.broadcast('there are... More?');
                         break;
 
