da73ba574c02f8a69d970e5fd30e7ea3f8d75f0c
Glitch (arrasiosiege) <none>
2022-04-05 22:09:54 +0000

    🥬🛣️ Updated with Glitch

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ const room = {
     room.findType('bas4');
     room.findType('roid');
     room.findType('rock');
-    room.findType('domx');
+    room.findType('bas6');
 
     room.nestFoodAmount = 1.5 * Math.sqrt(room.nest.length) / room.xgrid / room.ygrid;
     room.random = () => {
